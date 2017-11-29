86d32a2479000250410e26a647f835a9be145f4c
Ilya Klyuchnikov <ilyaklyuchnikov@fb.com>
2017-11-29 22:55:05 +0000

    simpler

--- TokenDef.hs
+++ TokenDef.hs
@@ -6,20 +6,13 @@ data TokenDefinition  = TokenDef
                { commentLine    :: String
                , identStart     :: Parser Char
                , identLetter    :: Parser Char
-               , opStart        :: Parser Char
-               , opLetter       :: Parser Char
                , reservedNames  :: [String]
-               , reservedOpNames:: [String]
                }
 
 henk  = TokenDef
     { commentLine  = "--"
     , identStart  = letter
     , identLetter  = alphaNum <|> oneOf "_'"
-    , opStart       = opLetter henk
-    , opLetter      = oneOf ":=\\->/|~.*[]"
-    , reservedOpNames = ["::","=","\\","->","=>","/\\","\\/"
-                        ,"|~|",".",":","*","[]"]
     , reservedNames = [ "case", "data", "letrec", "type"
           , "import", "in", "let", "of", "at", "Int"
           ]
